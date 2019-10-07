93a2d58b09611d202b228dfc0e4da37d012c578f
yuya <pana@DESKTOP-GMDCENL>
2019-10-07 15:50:26 +0900

    ASC.Web finished chapter2

--- ASC.Web/ASC.Web/Configuration/ApplicationSettings.cs
+++ ASC.Web/ASC.Web/Configuration/ApplicationSettings.cs
@@ -8,6 +8,8 @@ namespace ASC.Web.Configuration
     public class ApplicationSettings
     {
         public string ApplicationTitle { get; set; }
+        public string AnotherSetting { get; set; }
+        public string YetAnotherSetting { get; set; }
 
     }
 }
--- ASC.Web/ASC.Web/Controllers/HomeController.cs
+++ ASC.Web/ASC.Web/Controllers/HomeController.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASC.Web.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ASC.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        private IOptions<ApplicationSettings> _settings;
+
+        public HomeController(IOptions<ApplicationSettings> settings)
+        {
+            _settings = settings;
+        }
         public IActionResult Index()
         {
+            ViewBag.Title = _settings.Value.ApplicationTitle;
             return View();
         }
 
